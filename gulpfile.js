579886ece60bc122e7c4b8d40d4d9b7f3231a352
thierry rene matos <td_matos@terra.com.br>
2019-04-30 11:57:10 -0300

    primeiro commit - enviado o workflow ideal (finalmente)

---
+++ gulpfile.js
@@ -0,0 +1,107 @@
+/*global require*/
+"use strict";
+
+var gulp = require('gulp'),
+  path = require('path'),
+  data = require('gulp-data'),
+  pug = require('gulp-pug'),
+  prefix = require('gulp-autoprefixer'),
+  sass = require('gulp-sass'),
+  browserSync = require('browser-sync'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify');
+
+/*
+ * Directories here
+ */
+var paths = {
+  public: './public/',
+  sass:   './src/sass/',
+  css:    './public/css/',
+  data:   './src/_data/',
+  js:     './src/js/',
+  minJS:  './public/js/'
+};
+
+gulp.task('compressJS', function () {
+  return gulp.src(paths.js + '*.js')
+    .pipe(concat('group.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.minJS));
+});
+
+/**
+ * Compile .pug files and pass in data from json file
+ * matching file name. index.pug - index.pug.json
+ */
+gulp.task('pug', function () {
+  return gulp.src('./src/*.pug')
+    .pipe(data(function (file) {
+      return require(paths.data + path.basename(file.path) + '.json');
+    }))
+    .pipe(pug())
+    .on('error', function (err) {
+      process.stderr.write(err.message + '\n');
+      this.emit('end');
+    })
+    .pipe(gulp.dest(paths.public));
+});
+
+/**
+ * Recompile .pug files and live reload the browser
+ */
+gulp.task('rebuild', ['pug'], function () {
+  browserSync.reload();
+});
+
+/**
+ * Wait for pug and sass tasks, then launch the browser-sync Server
+ */
+gulp.task('browser-sync', ['sass', 'pug', 'compressJS'], function () {
+  browserSync({
+    server: {
+      baseDir: paths.public
+    },
+    notify: false
+  });
+});
+
+/**
+ * Compile .scss files into public css directory With autoprefixer no
+ * need for vendor prefixes then live reload the browser.
+ */
+gulp.task('sass', function () {
+  return gulp.src(paths.sass + '*.scss')
+    .pipe(sass({
+      includePaths: [paths.sass],
+      outputStyle: 'compressed'
+    }))
+    .on('error', sass.logError)
+    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+      cascade: true
+    }))
+    .pipe(gulp.dest(paths.css))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch .pug files run pug-rebuild then reload BrowserSync
+ */
+gulp.task('watch', function () {
+  gulp.watch(paths.sass + '**/*.scss', ['sass']);
+  gulp.watch('./src/**/*.pug', ['rebuild']);
+  gulp.watch('./src/**/*.js', ['compressJS', 'rebuild']);
+});
+
+// Build task compile sass and pug.
+gulp.task('build', ['sass', 'pug']);
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync then watch
+ * files for changes
+ */
+gulp.task('default', ['browser-sync', 'watch']);
---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+console.log('script.js carregado com sucesso :D');
+
+$(document).ready(function() {
+
+});
